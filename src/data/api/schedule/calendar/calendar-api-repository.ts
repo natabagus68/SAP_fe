a89015aaa9197d0abca764730214f9d816a87b3d
bima <bima.ragda@gmail.com>
2023-05-19 08:56:27 +0700

    calendar get data

--- src/data/api/schedule/calendar/calendar-api-repository.ts
+++ src/data/api/schedule/calendar/calendar-api-repository.ts
@@ -5,7 +5,7 @@ import { api } from "@data/api/_api";
 export class CalendarApiRepository implements CalendarRepository {
   async create(calendar: Calendar): Promise<Calendar> {
     try {
-      // const { data } = await api.post(`schedules`, calendar._props);
+      const { data } = await api.post(`schedules`, calendar._props);
       return Calendar.create({
         success: true,
         message: "Message success",
@@ -15,6 +15,21 @@ export class CalendarApiRepository implements CalendarRepository {
         success: false,
         message: error?.response?.data?.message,
       });
+    }
+  }
+  async get(month: number): Promise<Calendar[]> {
+    try {
+      const { data } = await api.get(`schedules?month=${month}`);
+      return data.data?.map((item) =>
+        Calendar.create({
+          success: true,
+          message: "Message success",
+        })
+      );
+      // return Calendar.create({
+      // });
+    } catch (error) {
+      return [];
     }
   }
 }
--- src/domain/repositories/schedule/calendar/calendar-repository.ts
+++ src/domain/repositories/schedule/calendar/calendar-repository.ts
@@ -2,4 +2,5 @@ import { Calendar } from "@domain/models/schedule/calendar/calendar";
 
 export interface CalendarRepository {
   create(calendar: Calendar): Promise<Calendar>;
+  get(month: number): Promise<Calendar[]>;
 }
